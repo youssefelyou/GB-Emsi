1bd68b4106f7d1865c4659c30a97d508534bf15b
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-11-20 17:54:10 +0100

    add classes

---
+++ FullScreenAppDemo/Classes/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullScreenAppDemo.Classes
+{
+    internal class Client
+    {
+        private int id;
+        private string fullname;
+        private string cin;
+        private bool active;
+
+        public Client(string fullname, string cin, bool active)
+        {
+           
+            this.Fullname = fullname;
+            this.Cin = cin;
+            this.Active = active;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Fullname { get => fullname; set => fullname = value; }
+        public string Cin { get => cin; set => cin = value; }
+        public bool Active { get => active; set => active = value; }
+    }
+}
